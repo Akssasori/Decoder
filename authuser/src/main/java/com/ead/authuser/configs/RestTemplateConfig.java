5131f950baa4deea7a0dedabc0a057425c41cdc5
Akssasori <diniz3003@gmail.com>
2022-10-01 10:46:19 -0300

    rest template

---
+++ authuser/src/main/java/com/ead/authuser/configs/RestTemplateConfig.java
@@ -0,0 +1,20 @@
+package com.ead.authuser.configs;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+
+    //Metodo produdor(igual o cliente quando usamos feign)
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+
+        return builder.build();
+    }
+
+
+}
