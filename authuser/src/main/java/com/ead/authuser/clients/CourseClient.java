4ea80422c115f20be732422cec7e420cf3e8650a
Akssasori <diniz3003@gmail.com>
2022-12-26 10:53:58 -0300

    circuit break

--- authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
+++ authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -4,6 +4,8 @@ import com.ead.authuser.dtos.CourseDto;
 import com.ead.authuser.dtos.ResponsePageDto;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UtilsService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,6 +20,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -35,6 +38,8 @@ public class CourseClient {
     @Value("${ead.api.url.course}")
     String REQUEST_URL_COURSE;
 
+//    @Retry(name = "retryInstance", fallbackMethod = "retryfallback")
+    @CircuitBreaker(name = "circuitbreakerInstance")
     public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable) {
 
         List<CourseDto> searchResult = null;
@@ -63,6 +68,12 @@ public class CourseClient {
         log.info("Ending request /courses userId {} ", userId);
 
         return result.getBody();
+    }
+
+    public Page<CourseDto> retryfallback(UUID userId, Pageable pageable,Throwable t) {
+        log.error("Inside retry retryfallback, cause - {}", t.toString());
+        List<CourseDto> searchResult = new ArrayList<>();
+        return new PageImpl<>(searchResult);
     }
 
 }
--- authuser/src/main/java/com/ead/authuser/configs/RestTemplateConfig.java
+++ authuser/src/main/java/com/ead/authuser/configs/RestTemplateConfig.java
@@ -6,16 +6,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @Configuration
 public class RestTemplateConfig {
 
+
+    public static final int TIMEOUT = 5000;
 
     //Metodo produdor(igual o cliente quando usamos feign)
     @LoadBalanced
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
 
-        return builder.build();
+        return builder.setConnectTimeout(Duration.ofMinutes(TIMEOUT)).setReadTimeout(Duration.ofMinutes(TIMEOUT)).build();
     }
 
 
