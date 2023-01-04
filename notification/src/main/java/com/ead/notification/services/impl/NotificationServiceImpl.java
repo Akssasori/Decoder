233a04e3732effcd371661c41e5d7400a0d0ca2f
Akssasori <diniz3003@gmail.com>
2023-01-04 09:39:25 -0300

    configurando o rabbit no notification

--- notification/src/main/java/com/ead/notification/services/impl/NotificationServiceImpl.java
+++ notification/src/main/java/com/ead/notification/services/impl/NotificationServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ead.notification.services.impl;
 
+import com.ead.notification.models.NotificationModel;
 import com.ead.notification.repositories.NotificationRepository;
 import com.ead.notification.services.NotificationService;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,8 @@ public class NotificationServiceImpl implements NotificationService {
     }
 
 
-
-
+    @Override
+    public NotificationModel saveNotification(NotificationModel notificationModel) {
+        return notificationRepository.save(notificationModel);
+    }
 }
